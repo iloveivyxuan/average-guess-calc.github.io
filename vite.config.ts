551bf1f3d8da54ab8f4f1164a2b509cd727ecbc9
xuan <iloveivyxuan@gmail.com>
2026-03-06 21:47:49 -0600

    Update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/average-guess-calc.github.io/',
+    base: '/average-guess-calc/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
